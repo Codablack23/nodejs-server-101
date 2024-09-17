e37727ab93724b4fdf6775102bedde86fdf79a88
Goodluck Edih <goodluckedih@yahoo.com>
2024-09-17 12:48:33 +0100

    added routing

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ import http from "http";
 
 const PORT = 5505
 
+const responses = {
+    "/":"This is is the home page",
+    "/about":"This is is the about page",
+    "/contact":"This is is the contact-us page",
+}
+
 const server = http.createServer( (req, res)=>{
-    res.end("Welcome to my Server")
+    const responseText = responses[req.url]
+    console.log(responseText)
+    if(responseText) {
+        return res.end(responseText)
+    }
+    res.end("This page does not exist")
 })
 server.listen(PORT,()=>{
-    console.log(`Server started at ${PORT}`)
+    console.log(`Server started at ${PORT} url: http://localhost:${PORT}`)
 })
 
 
